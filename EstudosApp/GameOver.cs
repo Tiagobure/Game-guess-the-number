a43f5535c7ff739ff1923ac6b659f40b2d02cda9
Tiago Santos Palma <tiagosantos.mr@gmail.com>
2020-08-02 12:28:08 -0300

    Game guess the number solution

---
+++ EstudosApp/GameOver.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+
+namespace EstudosApp
+{
+    [Activity(Label = "GameOver")]
+    public class GameOver : Activity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+
+            // Create your application here
+            SetContentView(Resource.Layout.GameOver);
+
+            TextView text_final_sorteado = FindViewById<TextView>(Resource.Id.text_final_sorteado);
+            TextView text_final_tentativas = FindViewById<TextView>(Resource.Id.text_final_tentativas);
+            Button cmd_terminar = FindViewById<Button>(Resource.Id.cmd_terminar);
+
+            int Sorteado = int.Parse(Intent.GetStringExtra("Sorteado"));
+            int Tentativas = int.Parse(Intent.GetStringExtra("Tentativas"));
+
+            text_final_sorteado.Text = "Número Sorteado" + Sorteado.ToString();
+
+            if (Tentativas == 0)
+            {
+                text_final_tentativas.Text = "Foi necessária uma tentativa";
+
+
+            }
+            else
+            {
+                text_final_tentativas.Text = "Foram necessárias " + Tentativas.ToString()+ " Tentativas para adivinhar.";
+            }
+
+            cmd_terminar.Click += delegate
+            {
+                this.Finish();
+            };
+           
+        }
+
+        
+    }
+}
---
+++ EstudosApp/MainGame.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Widget;
+
+namespace EstudosApp
+{
+    [Activity(Label = "MainGame", Theme = "@style/AppTheme", MainLauncher = true)]
+    public class MainGame : Activity
+    {
+        TextView text_tentativas;
+        EditText edit_valor;
+        Button cmd_adivinhar;
+        Button cmd_reiniciar;
+
+        int Valor_Sorteado;
+        int Numero_Tentativa;
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+
+            // Create your application here
+            SetContentView(Resource.Layout.MainGame);
+
+
+            text_tentativas = FindViewById<TextView>(Resource.Id.text_tentativas);
+            cmd_reiniciar = FindViewById<Button>(Resource.Id.cmd_reiniciar);
+            edit_valor = FindViewById<EditText>(Resource.Id.edit_valor);
+            cmd_adivinhar = FindViewById<Button>(Resource.Id.cmd_adivinhar);
+
+
+            Cmd_reiniciar_Click(this, EventArgs.Empty);
+            //first game
+            Random Rnd = new Random();
+            Valor_Sorteado = Rnd.Next(0, 1000);
+
+            //iniciar tentativas
+            Numero_Tentativa = 0;
+
+            cmd_adivinhar.Click += Cmd_adivinhar_Click;
+            cmd_reiniciar.Click += Cmd_reiniciar_Click;      
+
+
+
+        }
+
+        private void Cmd_reiniciar_Click(object sender, EventArgs e)
+        {
+            Random Rnd = new Random();
+            Valor_Sorteado = Rnd.Next(0, 1000);
+
+            //iniciar tentativas
+            Numero_Tentativa = 0;
+        }
+
+        private void Cmd_adivinhar_Click(object sender, EventArgs e)
+        {
+            //analisa dados inseridos
+            //se dado valido
+            if (edit_valor.Text == "" ) return;
+
+            int Valor_Inserido = int.Parse(edit_valor.Text);
+
+            string mensagen = "";
+
+            if (Valor_Inserido < Valor_Sorteado)
+            {
+                mensagen = "O valor inserido é inferior ao sorteado";
+                Atualizar_Tentativas();
+            }else if (Valor_Inserido > Valor_Sorteado)
+            {
+                mensagen = "O valor inserido é superior ao sorteado";
+                Atualizar_Tentativas();
+            }
+            else
+            {
+                //acertou
+                var AtividadeFinal = new Intent(this, typeof(GameOver));
+
+                AtividadeFinal.PutExtra("Sorteado", Valor_Sorteado.ToString());
+                AtividadeFinal.PutExtra("Tentativas", Numero_Tentativa.ToString());
+                StartActivity(AtividadeFinal);
+
+                return;
+
+            }
+            AlertDialog.Builder caixa = new AlertDialog.Builder(this);
+            caixa.SetTitle("Adivinha o número");
+            caixa.SetMessage(mensagen);
+            caixa.SetPositiveButton("OK", delegate { });
+            caixa.SetCancelable(false);
+            caixa.Show();
+
+        }
+        private void Atualizar_Tentativas()
+        {
+            Numero_Tentativa++;
+            text_tentativas.Text = "Tentativas: " + Numero_Tentativa.ToString();
+        }
+    }
+}
